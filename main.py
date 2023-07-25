28021ff4cd8e6927e58c2fe1c47fdee1f4cb0646
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2023-07-25 09:09:31 +0800

    change repos into 100

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def clean_content(content: str):
     return content
 
 
-def get_repository_data(query, max_repos=500):
+def get_repository_data(query, max_repos=100):
     blacklist = load_blacklist()
 
     # 设置分页参数
